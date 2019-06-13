0852e27c2ecbd6542d357a73354ecaf9d9566515
Robert O'Gara <rgo594@gmail.com>
2019-06-13 15:49:44 -0400

    no scaff backend

---
+++ back-end/app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
---
+++ back-end/config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'reviews/update'
+  get 'reviews/delete'
+  get 'movies/index'
+  get 'movies/show'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/update'
+  get 'movies/delete'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/update'
+  get 'users/delete'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
