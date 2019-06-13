ac651f924faa63da736c866d26284740431b5642
Robert O'Gara <rgo594@gmail.com>
2019-06-13 17:43:39 -0400

    added movie_img

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_193314) do
+ActiveRecord::Schema.define(version: 2019_06_13_214209) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_193314) do
     t.integer "avg_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "movie_img"
   end
 
   create_table "reviews", force: :cascade do |t|
