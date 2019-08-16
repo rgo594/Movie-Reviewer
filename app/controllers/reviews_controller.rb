4bb1a246047838ac079a849156e1a88e1034aef3
github username <github email address>
2019-08-16 16:57:39 -0400

    user created reviews now showing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,15 @@ class ReviewsController < ApplicationController
   skip_before_action :authorized
   skip_before_action :verify_authenticity_token
 
-  def user_reviews
-    @reviews = Review.select( |r| r[:user_id] == params[:user_id].to_i)
-    render json: {review: @reviews}
-  end
 
   def show
     @review = Review.select{ |r| r[:movie_id] == params[:id].to_i }
     render json: {review: @review}
+  end
+
+  def user_reviews
+    @reviews = Review.select{ |r| r[:user_id] == params[:id].to_i }
+    render json: {reviews: @reviews}
   end
 
   def new
